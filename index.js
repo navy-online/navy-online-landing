a7b7679855d07ce5ad33b152ce29ee820c598026
Justina Sokolova <Justinasal@yandex.ru>
2023-05-31 15:27:50 +0300

    feat: slider in about block

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
-const container = document.querySelector(".container");
-const slider = document.querySelector(".slider");
-const slidersCard = document.querySelectorAll(".slider_card");
-const btnPrev = document.querySelector("#btn-prev");
-const btnNext = document.querySelector("#btn-next");
-const slidesCount = slidersCard.length;
+const nftContainer = document.querySelector(".nft-container");
+const nftSlider = document.querySelector(".nft-slider");
+const nftSlidersCard = document.querySelectorAll(".nft-slider_card");
+const nftBtnPrev = document.querySelector("#nft-btn-prev");
+const nftBtnNext = document.querySelector("#nft-btn-next");
+const nftSlidesCount = nftSlidersCard.length;
 
-let activeSlideIndex = 0;
+const aboutContainer = document.querySelector(".about-container");
+const aboutSlider = document.querySelector(".about-slider");
+const aboutSlidersCard = document.querySelectorAll(".about-slider_card");
+const aboutBtnPrev = document.querySelector("#about-btn-prev");
+const aboutBtnNext = document.querySelector("#about-btn-next");
+const aboutSlidesCount = aboutSlidersCard.length;
 
-// slider.style.left = `-${(slidesCount - 1) * 100}vh`;
+// slider.style.left = `-${(nftSlidesCount - 1) * 100}vh`;
 
-btnPrev.addEventListener("click", () => {
-  changeSlide("left");
+let activeSlideIndexNft = 0;
+let activeSlideIndexAbout = 0;
+
+nftBtnPrev.addEventListener("click", () => {
+  changeSlideNft("left");
 });
 
-btnNext.addEventListener("click", () => {
-  changeSlide("right");
+nftBtnNext.addEventListener("click", () => {
+  changeSlideNft("right");
 });
 
-function changeSlide(direction) {
+aboutBtnPrev.addEventListener("click", () => {
+  changeSlideAbout("left");
+});
+
+aboutBtnNext.addEventListener("click", () => {
+  changeSlideAbout("right");
+});
+
+function changeSlideNft(direction) {
   if (direction === "right") {
-    activeSlideIndex++;
-    if (activeSlideIndex === slidesCount) {
-      activeSlideIndex = 0;
+    activeSlideIndexNft++;
+    if (activeSlideIndexNft === nftSlidesCount) {
+      activeSlideIndexNft = 0;
     }
   } else if (direction === "left") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesCount - 1;
+    activeSlideIndexNft--;
+    if (activeSlideIndexNft < 0) {
+      activeSlideIndexNft = nftSlidesCount - 1;
     }
   }
 
-  const width = container.clientWidth;
+  const width = nftContainer.clientWidth;
 
-  slider.style.transform = `translateX(-${activeSlideIndex * width}px`;
+  nftSlider.style.transform = `translateX(-${activeSlideIndexNft * width}px`;
+}
+
+function changeSlideAbout(direction) {
+  if (direction === "right") {
+    activeSlideIndexAbout++;
+    if (activeSlideIndexAbout === aboutSlidesCount) {
+      activeSlideIndexAbout = 0;
+    }
+  } else if (direction === "left") {
+    activeSlideIndexAbout--;
+    if (activeSlideIndexAbout < 0) {
+      activeSlideIndexAbout = aboutSlidesCount - 1;
+    }
+  }
+
+  const width = aboutContainer.clientWidth;
+
+  aboutSlider.style.transform = `translateX(-${
+    activeSlideIndexAbout * width
+  }px`;
 }
 
 history.pushState("", document.title, window.location.pathname); //https://www.navy.online
